663e7aa685050ed704fc75f8a0d04a2b99f1a8c1
24paketik <deniska-smirnov-05@mail.ru>
2022-04-27 13:15:04 +0300

    Пример 4.2

--- Company/Company/Cl_Company.cs
+++ Company/Company/Cl_Company.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace Company
 {
-    internal class Cl_Company
+    public class Cl_Company
     {
+        public string Name;
+        public int persons;
+        public int money;
+
+        public void show() // Печать информации о компании
+        {
+            Console.WriteLine("В компании {0} трудятся {1} сотрудников. Фонд заработной платы: {2}",Name, persons, money);
+        }
+        public int averageSalary() // Вычисление средней зарплаты
+        {
+            return money / persons;
+        }
+        public int minus(int minus1, int minus2) // Определение суммы налогов в ПФ
+        {
+            return money * (minus1 + minus2) / 100;
+        }
     }
+    class InsCompany : Cl_Company
+    {
+        int counts; //количество застрахованных лиц
+        int summaplus; // сумма страховых взносов
+        int summaminus; // сумма страховых выплат
+                        //-----------описание методов-----------------
+                        //вычисление среднего размера страховых взносов
+        public int averageInsSumma()
+        {
+            return summaplus / counts;
+        }
+    }; // конец описания класса InsCompany
+    class AvtoCompany : Cl_Company
+    {
+        int count; //количество автомобилей
+        int haul; // объём грузоперевозок
+                  //---- ------описание методов---------------
+                  // определение среднего объема грузоперевозок
+        public int averageAvto()
+        {
+            return haul / count;
+        }
+    } // конец описания класса AvtoCompany
 }
--- Company/Company/Program.cs
+++ Company/Company/Program.cs
@@ -7,10 +7,13 @@ namespace Company
         static void Main(string[] args)
         {
             // ввод данных
-
+            Cl_Company ob = new Cl_Company();
+            InsCompany ob1 = new InsCompany();
             // Обработка данных
 
             // Вывод данных
+            ob.show();
+            ob1.show();
         }
     }
 }
