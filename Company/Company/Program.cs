c201d6817d32327a5a56d384b970afd45bea55b4
24paketik <deniska-smirnov-05@mail.ru>
2022-04-27 13:00:35 +0300

    Create

---
+++ Company/Company/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Company
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ввод данных
+
+            // Обработка данных
+
+            // Вывод данных
+        }
+    }
+}
