004ead3e958f18668068fabf756b69929c4432f9
24paketik <deniska-smirnov-05@mail.ru>
2022-04-27 13:36:52 +0300

    Пример 4.5

---
+++ Company/Company/Company.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company
+{
+    public class Company
+    {
+        protected string name;
+        protected int persons;
+        protected int money;
+
+        public Company(string name, int persons, int money)
+        {
+            this.name = name;
+            this.persons = persons;
+            this.money = money;
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+        public void show() // Печать информации о компании
+        {
+            Console.WriteLine("В компании {0} трудятся {1} сотрудников. Фонд заработной платы: {2}",name, persons, money);
+        }
+        public int averageSalary() // Вычисление средней зарплаты
+        {
+            return money / persons;
+        }
+        public int maxPersons(int salary) // Средняя зарплата
+        {
+            return money / persons;
+        }
+        public int minus(int minus1, int minus2) // Определение суммы налогов в ПФ
+        {
+            return money * (minus1 + minus2) / 100;
+        }
+    }
+    class InsCompany : Company
+    {
+        int counts; //количество застрахованных лиц
+        int summaplus; // сумма страховых взносов
+        int summaminus; // сумма страховых выплат
+                        //-----------описание методов-----------------
+                        //вычисление среднего размера страховых взносов
+        public InsCompany(string name, int persons, int money, int counts, int summaplus, int summaminus) : base(name, persons, money)
+        {
+            this.counts = counts;
+            this.summaplus = summaplus;
+            this.summaminus = summaminus;
+        }
+        public int Summaplus //свойство
+        {
+            get
+            {
+                return summaplus;
+            }
+        }
+        // печать информации о страховой компании
+        new public void show()
+        {
+            base.show();
+            Console.WriteLine
+            ("застрахованных лиц " + counts +" взносы " + summaplus +" выплаты " + summaminus);
+        }
+        //вычисление среднего размера страховых взносов
+        public int averageIns()
+        {
+            return summaplus / counts;
+        }
+    }
+}
--- Company/Company/Program.cs
+++ Company/Company/Program.cs
@@ -6,14 +6,31 @@ namespace Company
     {
         static void Main(string[] args)
         {
-            // ввод данных
-            Cl_Company ob = new Cl_Company();
-            InsCompany ob1 = new InsCompany();
-            // Обработка данных
-
-            // Вывод данных
-            ob.show();
-            ob1.show();
+            int i, // параметр цикла
+                min, // наименьшая сумма страховыхвзносов
+                temp, j; //вспомогательные переменные
+            InsCompany[] CityCompany = new InsCompany[4];
+            // Создание и инициализация объектов
+            CityCompany[0] = new InsCompany
+            ("ABC", 100, 100000, 111, 111111, 1111);
+            CityCompany[1] = new InsCompany
+            ("BCD", 200, 200000, 222, 222222, 2222);
+            CityCompany[2] = new InsCompany
+            ("CDE", 300, 300000, 333, 333333, 3333);
+            CityCompany[3] = new InsCompany
+            ("DEF", 400, 400000, 444, 444444, 4444);
+            // печать информации о страховых компаниях
+            min = CityCompany[0].Summaplus; j = 0;
+            for (i = 1;i<4; i ++)
+            {
+                temp = CityCompany[i].Summaplus;
+                if (temp<min)
+                {
+                    min = temp;
+                    j= i;
+                }
+            }
+            Console.WriteLine("\n наименьшая сумма взносов в компании " + CityCompany[j].Name);
         }
     }
 }
